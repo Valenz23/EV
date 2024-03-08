df2de9d42992010cd7b51859aa1ac480c3dd19b7
Pablo Valenzuela Álvarez <valenbetis@gmail.com>
2024-03-08 13:37:32 +0100

    comit P1

--- Practica 1/camaraLeftRight.gd
+++ Practica 1/camaraLeftRight.gd
@@ -3,23 +3,17 @@ extends Node3D
 var velocidad = 5
 
 var rotacionY = 0
-var minY = -180
-var maxY = 180
-
-var rotacionZ = 0
-var minZ = -45
-var maxZ = 45
+var minY = -270
+var maxY = 270
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):	
 
 	var movimientoY = 0
-	var movimientoZ = 0
 	
 	if Input.is_action_pressed("camara izquierda"):
 		if rotacionY > minY:
@@ -30,19 +24,8 @@ func _process(delta):
 		if rotacionY < maxY:
 			movimientoY += velocidad * delta
 			rotacionY += velocidad
-		
-	if Input.is_action_pressed("camara arriba"):
-		if rotacionZ < maxZ:
-			movimientoZ += velocidad * delta
-			rotacionZ += velocidad
-		
-	if Input.is_action_pressed("camara abajo"):
-		if rotacionZ > minZ:
-			movimientoZ -= velocidad * delta
-			rotacionZ -= velocidad	
 			
-	print("y= ", rotacionY," z= ", rotacionZ)
-
+	#print(rotacionY)
 	
 	rotate_y(movimientoY)
-	rotate_z(movimientoZ)
+
